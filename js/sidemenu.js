3922349a1d2cc5bb67098d2edd1f9854151cf357
Jia Kuei <jackson1991@jredu.net>
2022-05-06 14:10:45 +0800

    sideBar by home_html finished

--- js/sidemenu.js
+++ js/sidemenu.js
@@ -1,4 +1,19 @@
-$("#leftside-navigation .sub-menu > a").click(function (e) {
-    $("#leftside-navigation ul ul").slideUp(), $(this).next().is(":visible") || $(this).next().slideDown(),
-        e.stopPropagation()
+// $("#leftside-navigation .sub-menu > a").click(function (e) {
+//     $("#leftside-navigation ul ul").slideUp(), $(this).next().is(":visible") || $(this).next().slideDown(),
+//         e.stopPropagation()
+// })
+
+const accordions = document.querySelectorAll('.sideBar_accordion');
+
+accordions.forEach(acc => {
+    acc.addEventListener("click", () => {
+        console.log(acc);
+        acc.classList.toggle("active");
+        const panel = acc.nextElementSibling;
+        if (panel.style.maxHeight) {
+            panel.style.maxHeight = null;
+        } else {
+            panel.style.maxHeight = panel.scrollHeight + "px";
+        }
+    })
 })
