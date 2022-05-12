9582cf93bc5b123a4ae33968e7f2ba9ddf81db13
Jia Kuei <jackson1991@jredu.net>
2022-05-12 10:32:19 +0800

    PreDepositManagement first finished

---
+++ js/preDepositManagement.js
@@ -0,0 +1,8 @@
+// const toggles = document.querySelectorAll(".list_table_toggle");
+
+
+// toggles.forEach(toggle => {
+//     toggle.addEventListener("click", () => {
+//         toggle.parentNode.parentNode.nextElementSibling.classList.toggle("active")
+//     })
+// })
