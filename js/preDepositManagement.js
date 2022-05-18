0c600fe68b7932d03f60e29496844e4f5569873f
Jia Kuei <jackson1991@jredu.net>
2022-05-18 14:22:06 +0800

    sideBar adjusted 20220518

--- js/preDepositManagement.js
+++ js/preDepositManagement.js
@@ -3,6 +3,8 @@
 const toggleRows = document.querySelectorAll(".content_table_tbody_tr");
 const tdListTables = document.querySelectorAll(".td_list_table");
 
+const btns = document.querySelectorAll(".btn")
+
 
 toggleRows.forEach(toggleRow => {
     toggleRow.addEventListener("click", () => {
@@ -16,3 +18,9 @@ function removeListTableActive() {
         tdListTable.classList.remove("List_table_active");
     })
 }
+
+btns.forEach(btn => {
+    btn.addEventListener("click", (e) => {
+        e.stopPropagation();
+    })
+})
