27d3f25b2e37505bb112996059411ca1715782a9
Jia Kuei <jackson1991@jredu.net>
2022-05-19 16:43:41 +0800

    First sideBar Menu Hide 20220519

--- js/sidemenu.js
+++ js/sidemenu.js
@@ -3,11 +3,13 @@
 //         e.stopPropagation()
 // })
 
+
+//側邊選單摺疊function
 const accordions = document.querySelectorAll('.sideBar_accordion');
 
 accordions.forEach(acc => {
     acc.addEventListener("click", () => {
-        console.log(acc);
+        // console.log(acc);
         acc.classList.toggle("sideBar_active");
         const panel = acc.nextElementSibling;
         if (panel.style.maxHeight) {
@@ -18,20 +20,27 @@ accordions.forEach(acc => {
     })
 })
 
-console.log(screen.width);
 
 
+//打開側邊選單function
 
-const hideBtn = document.querySelector('.openHideSideBar');
+const openHideSideBar = document.querySelector('.openHideSideBar');
 const sideBarMenu = document.querySelector('.sidebar-menu');
 const rightContant = document.querySelector('.right_content');
-console.log(hideBtn);
-hideBtn.addEventListener('click', () => {
+// console.log(hideBtn);
+openHideSideBar.addEventListener('click', () => {
     sideBarMenu.style.width = "240px";
     sideBarMenu.style.overflow = "visible";
-    // rightContant.style.marginLeft = '240px';
-})
+    rightContant.style.marginLeft = '240px';
+    openHideSideBar.style.display = "none !important";
 
+
+})
+console.log(openHideSideBar);
+
+//關閉側邊選單
+
+//側邊隱藏按鈕
 const sidebarHideBtn = document.querySelector('.sidebarHideBtn');
 
 sidebarHideBtn.addEventListener('click', () => {
@@ -39,18 +48,59 @@ sidebarHideBtn.addEventListener('click', () => {
     if (screen.width > 1440) {
         sideBarMenu.style.width = "240px";
         sideBarMenu.style.overflow = "visible";
+        rightContant.style.marginLeft = '240px';
+        openHideSideBar.style.display = "none !important";
         console.log('screen>1440')
     } else {
         sideBarMenu.style.width = "0px";
         sideBarMenu.style.overflow = "hidden";
+        rightContant.style.marginLeft = '0px';
+        openHideSideBar.style.display = "flex !important";
         console.log('screen <1440')
     }
 
 })
-
+//偵測螢幕寬度是否有改變，改動側邊選單的值
 window.onresize = function () {
     sideBarMenu.style.width = "240px";
     sideBarMenu.style.overflow = "visible";
-    console.log('screen>1440')
+    rightContant.style.marginLeft = '240px';
+    openHideSideBar.style.display = "none !important";
+    // console.log('screen>1440')
 }
 
+//點選側邊選單項目後側邊選單進行收回
+
+const panelLists = document.querySelectorAll(".panelList");
+
+// console.log(panelLists)
+
+
+panelLists.forEach(panelList => {
+    // console.log(panelList.children);
+    // console.log(123456);
+    // console.log(screen.width === "240px");
+    [...panelList.children].forEach(panelListLi => {
+        // console.log(panelListLi.children[0]);
+        // && sideBarMenu.style.width === "240px"
+
+        if (screen.width != 1920) {
+            panelListLi.children[0].addEventListener('click', function () {
+                sideBarMenu.style.width = "0px";
+                sideBarMenu.style.overflow = "hidden";
+                rightContant.style.marginLeft = '0px';
+                openHideSideBar.style.display = "flex !important";
+                console.log('123');
+
+                //把打開的手風琴收回
+                accordions.forEach(acc => {
+                    acc.classList.remove("sideBar_active");
+                    const panel = acc.nextElementSibling;
+                    if (panel.style.maxHeight) {
+                        panel.style.maxHeight = null;
+                    }
+                })
+            })
+        }
+    })
+})
