9e871713dbc042cb54827d13fbc123d47eb7995e
jiaKuei <momonbin1991@icloud.com>
2022-06-16 17:51:40 +0800

    '20220616'

--- js/preDepositManagement.js
+++ js/preDepositManagement.js
@@ -3,24 +3,44 @@
 const toggleRows = document.querySelectorAll(".content_table_tbody_tr");
 const tdListTables = document.querySelectorAll(".td_list_table");
 
-const btns = document.querySelectorAll(".btn")
+const btns = document.querySelectorAll(".content-table .btn");
+
+const contentTableInputs = document.querySelectorAll('.content-table input[type="checkbox"]');
+
 
 
 toggleRows.forEach(toggleRow => {
     toggleRow.addEventListener("click", () => {
-        toggleRow.classList.toggle("active_row");
-        toggleRow.nextElementSibling.querySelector('.td_list_table').classList.toggle("List_table_active");
+
+        if (toggleRow.classList.contains('active_row')) {
+            toggleRow.classList.toggle("active_row");
+            toggleRow.nextElementSibling.querySelector('.td_list_table').classList.toggle("List_table_active");
+        } else {
+            removeListTableActive();
+
+            toggleRow.classList.toggle("active_row");
+            toggleRow.nextElementSibling.querySelector('.td_list_table').classList.toggle("List_table_active");
+        }
+
     })
 })
 
 function removeListTableActive() {
     tdListTables.forEach(tdListTable => {
         tdListTable.classList.remove("List_table_active");
+        console.log("test", tdListTable.parentNode.previousElementSibling)
+        tdListTable.parentNode.previousElementSibling.classList.remove("active_row");
     })
 }
 
 btns.forEach(btn => {
     btn.addEventListener("click", (e) => {
+        e.stopPropagation();
+    })
+})
+
+contentTableInputs.forEach(input => {
+    input.addEventListener("click", (e) => {
         e.stopPropagation();
     })
 })
