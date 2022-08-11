7b37e263aca13906fc999992bf7a91ea89e34c61
jiaKuei <momonbin1991@icloud.com>
2022-08-11 17:48:33 +0800

    '20220811客戶問題單第二次存檔'

--- js/rightClickMenu.js
+++ js/rightClickMenu.js
@@ -1,41 +1,78 @@
-(function () {
-	$('.chat-msg-text').bind('contextmenu', function (ct) {
-		ct.preventDefault();
-		context = $('.context-menu');
+// (function () {
+// 	$('.chat-msg-text').bind('contextmenu', function (ct) {
+// 		ct.preventDefault();
+// 		context = $('.context-menu');
 
-		dw = parseInt($(window).width());
-		dh = parseInt($(window).height());
-		ctw = parseInt(ct.pageX);
-		cth = parseInt(ct.pageY);
+// 		dw = parseInt($(window).width());
+// 		dh = parseInt($(window).height());
+// 		ctw = parseInt(ct.pageX);
+// 		cth = parseInt(ct.pageY);
 
-		objw = context.width() + 5;
-		objh = context.height() + 5;
+// 		objw = context.width() + 5;
+// 		objh = context.height() + 5;
 
-		context.hide();
-		$('h1').remove();
+// 		context.hide();
+// 		$('h1').remove();
 
-		if (dw - ctw < objw) {
-			context
-				.css('position', 'absolute')
-				.css('top', ct.pageY + 'px')
-				.css('left', dw - objw + 'px')
-				.slideToggle();
-		} else if (dh - cth < objh) {
-			context
-				.css('position', 'absolute')
-				.css('top', dh - objh + 'px')
-				.css('left', ct.pageX + 'px')
-				.slideToggle();
+// 		if (dw - ctw < objw) {
+// 			context
+// 				.css('position', 'absolute')
+// 				.css('top', ct.pageY + 'px')
+// 				.css('left', dw - objw + 'px')
+// 				.slideToggle();
+// 		} else if (dh - cth < objh) {
+// 			context
+// 				.css('position', 'absolute')
+// 				.css('top', dh - objh + 'px')
+// 				.css('left', ct.pageX + 'px')
+// 				.slideToggle();
+// 		} else {
+// 			context
+// 				.css('position', 'absolute')
+// 				.css('top', ct.pageY + 'px')
+// 				.css('left', ct.pageX + 'px')
+// 				.slideToggle();
+// 		}
+// 	});
+
+// 	$(document).bind('click', function () {
+// 		context.hide();
+// 	});
+// })();
+const chatMsgTexts = document.querySelectorAll('.chat-msg-text');
+
+chatMsgTexts.forEach(chatMsgText => {
+	chatMsgText.oncontextmenu = function (e) {
+		e.preventDefault();
+		if (!document.getElementById('right-menu').contains(e.target)) {
+			document.getElementById('right-menu').style.top = e.clientY + 'px';
+			document.getElementById('right-menu').style.left = e.clientX + 'px';
+
+			document.getElementById('right-menu').style.display = 'block';
+		}
+	};
+
+	window.addEventListener('click', function (e) {
+		if (!document.getElementById('right-menu').contains(e.target)) {
+			document.getElementById('right-menu').style.display = 'none';
 		} else {
-			context
-				.css('position', 'absolute')
-				.css('top', ct.pageY + 'px')
-				.css('left', ct.pageX + 'px')
-				.slideToggle();
+			document.getElementById('right-menu').style.display = 'none';
 		}
 	});
+});
 
-	$(document).bind('click', function () {
-		context.hide();
-	});
-})();
+// window.oncontextmenu = function (e) {
+// 	e.preventDefault();
+// 	if (!document.getElementById('right-menu').contains(e.target)) {
+// 		document.getElementById('right-menu').style.top = e.clientY + 'px';
+// 		document.getElementById('right-menu').style.left = e.clientX + 'px';
+
+// 		document.getElementById('right-menu').style.display = 'block';
+// 	}
+// };
+
+// window.addEventListener('click', function (e) {
+// 	if (!document.getElementById('right-menu').contains(e.target)) {
+// 		document.getElementById('right-menu').style.display = 'none';
+// 	}
+// });
