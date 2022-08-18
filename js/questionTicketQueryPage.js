e0455ced48c9f4c1ae8a52c8dec49bf9c2d7489c
jiaKuei <momonbin1991@icloud.com>
2022-08-18 09:37:13 +0800

    '20220818客戶問題單第三次存檔'

---
+++ js/questionTicketQueryPage.js
@@ -0,0 +1,38 @@
+// $('#cramSchoolNameSearch').select2({
+// 	theme: 'bootstrap-5',
+// 	width: $(this).data('width')
+// 		? $(this).data('width')
+// 		: $(this).hasClass('w-100')
+// 		? '100%'
+// 		: 'style',
+// 	placeholder: $('#cramSchoolNameSearch').data('placeholder'),
+// });
+$('#cramSchoolNameSearch').select2({
+	theme: 'bootstrap-5',
+	width: $('#cramSchoolNameSearch').data('width')
+		? $('#cramSchoolNameSearch').data('width')
+		: $('#cramSchoolNameSearch').hasClass('w-100')
+		? '100%'
+		: 'style',
+	placeholder: $('#cramSchoolNameSearch').data('placeholder'),
+});
+
+$('#selectCramSchoolCounty').select2({
+	theme: 'bootstrap-5',
+	width: $('#selectCramSchoolCounty').data('width')
+		? $('#selectCramSchoolCounty').data('width')
+		: $('#selectCramSchoolCounty').hasClass('w-100')
+		? '100%'
+		: 'style',
+	placeholder: $('#selectCramSchoolCounty').data('placeholder'),
+});
+
+$('#selectCramSchoolAdministrativeArea').select2({
+	theme: 'bootstrap-5',
+	width: $('#selectCramSchoolAdministrativeArea').data('width')
+		? $('#selectCramSchoolAdministrativeArea').data('width')
+		: $('#selectCramSchoolAdministrativeArea').hasClass('w-100')
+		? '100%'
+		: 'style',
+	placeholder: $('#selectCramSchoolAdministrativeArea').data('placeholder'),
+});
